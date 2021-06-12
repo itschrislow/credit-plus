bf39ed3532efbdcfe8c581c2fcc641e9a9f6edee
chris0610 <chrislow360@gmail.com>
2021-06-12 16:40:39 +0800

    add chatbot styling

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -9,8 +9,44 @@ import {
 } from "src/products";
 
 const Chatbot = () => {
+  const [state, setState] = useState(false);
+  const toggle = ({ opened }) => setState({ opened });
+
   return (
     <ReactChatBot
+      botDelay={500}
+      userDelay={500}
+      floating={true}
+      opened={state}
+      toggleFloating={toggle}
+      headerTitle="Rob"
+      style={{ fontFamily: "sans-serif" }}
+      bubbleStyle={{ color: "white", backgroundColor: "#0464b4", fontSize: "16px" }}
+      bubbleOptionStyle={{ backgroundColor: "#0464b4" }}
+      floatingStyle={{ backgroundColor: "#0464b4" }}
+      submitButtonStyle={{ height: "57px" }}
+      headerComponent={
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            height: "56px",
+            paddingLeft: "20px",
+            paddingRight: "10px",
+            color: "white",
+            backgroundColor: "#0464b4",
+            fontSize: "16px"
+          }}
+        >
+          <h2 className="sc-crzoAE kYtdwL rsc-header-title">
+            Rob
+          </h2>
+          <a onClick={() => setState(false)} style={{ cursor: "pointer" }}>
+            <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path fill="#fff" d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path><path d="M0 0h24v24H0z" fill="none"></path></svg>
+          </a>
+        </div>
+      }
       steps={[
         {
           id: "q1",

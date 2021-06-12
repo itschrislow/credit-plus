3211ec2a1daca5b881d77909289e840244f5d8bf
chris0610 <chrislow360@gmail.com>
2021-06-12 22:00:57 +0800

    update styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Nav = () => {
   return (
-    <nav className="z-10 h-10 sm:h-20 py-2 sm:py-0 w-full px-10 flex items-center">
+    <nav className="z-10 sticky top-0 h-10 sm:h-20 py-2 sm:py-0 w-full px-10 flex items-center bg-white">
       <Link href="/">
         <img src="/static/images/rhb-logo.png" className="cursor-pointer" />
       </Link>

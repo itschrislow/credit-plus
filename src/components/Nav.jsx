a53ab4d9ff8f814caa8a03a26f762e3f0e7b30f7
chris0610 <chrislow360@gmail.com>
2021-06-12 21:19:57 +0800

    update nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,30 +2,10 @@ import Link from "next/Link";
 
 const Nav = () => {
   return (
-    <nav className="z-10 h-20 w-full px-10 flex items-center">
-      <img src="/static/images/rhb-logo.png" />
-      {/* <div className="w-full flex justify-evenly font-light">
-        <Link href="/">
-          <h1 className="cursor-pointer hover:underline">
-            BizPower SME Business Loan
-          </h1>
-        </Link>
-        <Link href="/">
-          <h1 className="cursor-pointer hover:underline">
-            BizPlus/-i
-          </h1>
-        </Link>
-        <Link href="/">
-          <h1 className="cursor-pointer hover:underline">
-            BizPower SME Property Loan
-          </h1>
-        </Link>
-        <Link href="/">
-          <h1 className="cursor-pointer hover:underline">
-            BizPower Relief Financing
-          </h1>
-        </Link>
-      </div> */}
+    <nav className="z-10 h-10 sm:h-20 py-2 sm:py-0 w-full px-10 flex items-center">
+      <Link href="/">
+        <img src="/static/images/rhb-logo.png" className="cursor-pointer" />
+      </Link>
     </nav>
   )
 }

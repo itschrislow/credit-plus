82af5422562f87719d2f0d8dfa62b8075c4849b2
Shen Wei Leong <shenwei0102@gmail.com>
2021-06-11 22:57:20 -0400

    Update

---
+++ pages/modules/profile.js
@@ -0,0 +1,22 @@
+
+
+export default class profile {
+    constructor(balance, income, SSM) {
+        this.balance = balance
+        this.income = income
+        this.SSM = SSM
+    }
+
+    calculateApprovalRate() {
+        
+    }
+
+    getCompanyCTOSScore() {
+        // pull some score from db
+        
+    }
+
+    getCompanyCCRISScore() {
+
+    }
+}

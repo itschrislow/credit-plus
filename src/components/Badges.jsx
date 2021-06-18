b978f832454ed3ebfbe2c18bf3a919f06133d254
chris0610 <chrislow360@gmail.com>
2021-06-18 16:30:04 +0800

    add badges component

---
+++ src/components/Badges.jsx
@@ -0,0 +1,17 @@
+export const RedBadge = ({ children }) => (
+  <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-red-100 text-red-800">
+    {children}
+  </span>
+)
+
+export const YellowBadge = ({ children }) => (
+  <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-yellow-100 text-yellow-800">
+    {children}
+  </span>
+)
+
+export const GreenBadge = ({ children }) => (
+  <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800">
+    {children}
+  </span>
+)

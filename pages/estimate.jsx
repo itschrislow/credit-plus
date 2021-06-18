b7f604371042def94872ddc7a3f0ac1627452edc
Shen Wei Leong <shenwei0102@gmail.com>
2021-06-18 14:54:53 -0400

    update

--- pages/estimate.jsx
+++ pages/estimate.jsx
@@ -99,7 +99,7 @@ const Scores = ({ scores }) => {
       </div>
       <div className="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r">
         <dt className="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
-          Credit Score
+          Credit History
         </dt>
         <dd className="order-1 text-5xl font-extrabold text-darkBlue">
           {scores?.credit}%

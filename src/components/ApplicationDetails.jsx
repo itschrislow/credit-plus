33c2d1af829a0f793e5b113b48e4a3bd71609d45
chris0610 <chrislow360@gmail.com>
2021-06-18 16:33:46 +0800

    update application details fields

--- src/components/ApplicationDetails.jsx
+++ src/components/ApplicationDetails.jsx
@@ -1,7 +1,8 @@
 import { Fragment } from 'react'
+
 import { XIcon } from '@heroicons/react/outline'
-import { Dialog, Menu, Transition } from '@headlessui/react'
-import { DotsVerticalIcon } from '@heroicons/react/solid'
+import { Dialog, Transition } from '@headlessui/react'
+import { GreenBadge, YellowBadge, RedBadge } from './Badges'
 
 const ApplicationDetails = ({ showDetails, setShowDetails }) => {
   const closeModal = () => setShowDetails(false)
@@ -13,7 +14,7 @@ const ApplicationDetails = ({ showDetails, setShowDetails }) => {
         className="fixed inset-0 z-10 overflow-y-auto"
         onClose={closeModal}
       >
-        <div className="min-h-screen px-4 text-center">
+        <div className="min-h-screen px-4 bg-gray-700 bg-opacity-80">
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -26,14 +27,7 @@ const ApplicationDetails = ({ showDetails, setShowDetails }) => {
             <Dialog.Overlay className="fixed inset-0" />
           </Transition.Child>
 
-          {/* This element is to trick the browser into centering the modal contents. */}
-          <span
-            className="inline-block h-screen align-middle"
-            aria-hidden="true"
-          >
-            &#8203;
-            </span>
-          <div className="fixed inset-y-0 right-0 pl-10 max-w-full flex">
+          <div className="fixed inset-y-0 right-0 pl-10 max-w-full flex sm:pl-16">
             <Transition.Child
               as={Fragment}
               enter="transform transition ease-in-out duration-500 sm:duration-700"
@@ -45,14 +39,14 @@ const ApplicationDetails = ({ showDetails, setShowDetails }) => {
             >
               <div className="w-screen max-w-5xl">
                 <div className="h-full flex flex-col bg-white shadow-xl overflow-y-scroll">
-                  <div className="px-4 py-6 sm:px-6">
-                    <div className="flex items-start justify-between">
-                      <h2 id="slide-over-heading" className="text-lg font-medium text-gray-900">
-                        Profile
-                      </h2>
+                  <div className="px-4 sm:px-6">
+                    <div className="h-16 flex items-center justify-between">
+                      <Dialog.Title className="text-lg font-medium text-gray-900">
+                        {/* Company Name */}
+                      </Dialog.Title>
                       <div className="ml-3 h-7 flex items-center">
                         <button
-                          className="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:ring-2 focus:ring-indigo-500"
+                          className="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none"
                           onClick={closeModal}
                         >
                           <span className="sr-only">Close panel</span>
@@ -62,128 +56,156 @@ const ApplicationDetails = ({ showDetails, setShowDetails }) => {
                     </div>
                   </div>
                   {/* Main */}
-                  <div>
-                    <div className="pb-1 sm:pb-6">
-                      <div className="mt-6 px-4 sm:mt-8 sm:flex sm:items-end sm:px-6">
+                  <div className="divide-y divide-gray-200">
+                    <div className="pb-6">
+                      <div className="flow-root px-4 sm:flex sm:items-end sm:px-6">
                         <div className="sm:flex-1">
                           <div>
-                            <div className="flex items-center">
-                              <h3 className="font-bold text-xl text-gray-900 sm:text-2xl">Ashley Porter</h3>
-                              <span className="ml-2.5 bg-green-400 flex-shrink-0 inline-block h-2 w-2 rounded-full">
-                                <span className="sr-only">Online</span>
-                              </span>
-                            </div>
-                            <p className="text-sm text-gray-500">@ashleyporter</p>
+                            <h3 className="font-bold text-xl text-gray-900 sm:text-2xl">
+                              Visibl Solutions
+                              </h3>
+                            <p className="text-sm text-gray-500">
+                              202003315827 (003190749-H)
+                            </p>
                           </div>
-                          <div className="mt-5 flex flex-wrap space-y-3 sm:space-y-0 sm:space-x-3">
+                          <div className="mt-5 flex justify-between">
                             <button
                               type="button"
-                              className="flex-shrink-0 w-full inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:flex-1"
+                              className="inline-flex items-center px-3.5 py-2 border border-transparent text-sm leading-4 font-medium rounded-lg shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                             >
-                              Message
+                              Compare
+                            </button>
+                            <div className="flex space-x-3">
+                              <button
+                                type="button"
+                                className="inline-flex items-center px-3.5 py-2 border border-transparent text-sm leading-4 font-medium rounded-lg shadow-sm text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+                              >
+                                Approve
                               </button>
-                            <button
-                              type="button"
-                              className="flex-1 w-full inline-flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                            >
-                              Call
+                              <button
+                                type="button"
+                                className="inline-flex items-center px-3.5 py-2 border border-transparent text-sm leading-4 font-medium rounded-lg shadow-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                              >
+                                Reject
                               </button>
-                            <span className="ml-3 inline-flex sm:ml-0">
-                              <Menu as="div" className="relative inline-block text-left">
-                                {({ open }) => (
-                                  <>
-                                    <Menu.Button className="inline-flex items-center p-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-400 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                      <span className="sr-only">Open options menu</span>
-                                      <DotsVerticalIcon className="h-5 w-5" aria-hidden="true" />
-                                    </Menu.Button>
-                                    <Transition
-                                      show={open}
-                                      as={Fragment}
-                                      enter="transition ease-out duration-100"
-                                      enterFrom="transform opacity-0 scale-95"
-                                      enterTo="transform opacity-100 scale-100"
-                                      leave="transition ease-in duration-75"
-                                      leaveFrom="transform opacity-100 scale-100"
-                                      leaveTo="transform opacity-0 scale-95"
-                                    >
-                                      <Menu.Items
-                                        static
-                                        className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
-                                      >
-                                        <div className="py-1">
-                                          <Menu.Item>
-                                            {({ active }) => (
-                                              <a
-                                                href="#"
-                                                className={classNames(
-                                                  active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                                  'block px-4 py-2 text-sm'
-                                                )}
-                                              >
-                                                View profile
-                                              </a>
-                                            )}
-                                          </Menu.Item>
-                                          <Menu.Item>
-                                            {({ active }) => (
-                                              <a
-                                                href="#"
-                                                className={classNames(
-                                                  active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                                  'block px-4 py-2 text-sm'
-                                                )}
-                                              >
-                                                Copy profile link
-                                              </a>
-                                            )}
-                                          </Menu.Item>
-                                        </div>
-                                      </Menu.Items>
-                                    </Transition>
-                                  </>
-                                )}
-                              </Menu>
-                            </span>
+                            </div>
                           </div>
                         </div>
                       </div>
                     </div>
-                    <div className="px-4 pt-5 pb-5 sm:px-0 sm:pt-0">
-                      <dl className="space-y-8 px-4 sm:px-6 sm:space-y-6">
-                        <div>
-                          <dt className="text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0">Bio</dt>
-                          <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">
-                            <p>
-                              Enim feugiat ut ipsum, neque ut. Tristique mi id elementum praesent. Gravida in tempus
-                              feugiat netus enim aliquet a, quam scelerisque. Dictumst in convallis nec in bibendum
-                              aenean arcu.
-                            </p>
-                          </dd>
+
+                    <div className="px-4 py-5 sm:px-0 sm:py-0">
+                      <div className="space-y-8 sm:divide-y sm:divide-gray-200 sm:space-y-0">
+                        {/* FINANCIAL STANDING */}
+                        <div className="sm:flex sm:px-6 sm:py-5">
+                          <p className="text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0 lg:w-48">
+                            Financial Standing
+                          </p>
+                          <div className="w-full mt-1 text-sm text-gray-900 sm:mt-0 sm:ml-6">
+                            <div className="w-full grid grid-cols-3 gap-4">
+                              <div className="col-span-1">
+                                <p>Balance Sheet</p>
+                                <GreenBadge>
+                                  85%
+                                </GreenBadge>
+                              </div>
+                              <div className="col-span-1">
+                                <p>Income Statement</p>
+                                <GreenBadge>
+                                  85%
+                                </GreenBadge>
+                              </div>
+                              <div className="col-span-1">
+                                <p>Liquidity Ratios</p>
+                                <RedBadge>
+                                  34%
+                                </RedBadge>
+                              </div>
+                              <div className="col-span-1">
+                                <p>
+                                  Profitability Ratios
+                                </p>
+                                <GreenBadge>
+                                  85%
+                                </GreenBadge>
+                              </div>
+                              <div className="col-span-1">
+                                <p>
+                                  Other Ratios
+                                </p>
+                                <YellowBadge>
+                                  55%
+                                </YellowBadge>
+                              </div>
+                            </div>
+                          </div>
                         </div>
-                        <div>
-                          <dt className="text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0">Location</dt>
-                          <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">New York, NY, USA</dd>
+                        {/* PAYMENT BEHAVIOUR */}
+                        <div className="sm:flex sm:px-6 sm:py-5">
+                          <p className="text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0 lg:w-48">
+                            Credit Report
+                          </p>
+                          <div className="w-full mt-1 text-sm text-gray-900 sm:mt-0 sm:ml-6">
+                            <div className="w-full grid grid-cols-3 gap-4">
+                              <div className="col-span-1">
+                                <p>CTOS SME Score</p>
+                                <GreenBadge>
+                                  368
+                                </GreenBadge>
+                              </div>
+                              <div className="col-span-1">
+                                <p>Conduct of Account</p>
+                                <RedBadge>
+                                  Irregular
+                                </RedBadge>
+                              </div>
+                              <div className="col-span-1">
+                                <p>Payment Frequency</p>
+                                <YellowBadge>
+                                  Regular
+                                </YellowBadge>
+                              </div>
+                            </div>
+                          </div>
                         </div>
-                        <div>
-                          <dt className="text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0">Website</dt>
-                          <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">ashleyporter.com</dd>
+                        {/* BUSINESS VIABILITY */}
+                        <div className="sm:flex sm:px-6 sm:py-5">
+                          <p className="text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0 lg:w-48">
+                            Business Viability
+                          </p>
+                          <div className="w-full mt-1 text-sm text-gray-900 sm:mt-0 sm:ml-6">
+                            <div className="w-full grid grid-cols-3 gap-4">
+                              <div className="col-span-1">
+                                <p>Sector</p>
+                                <GreenBadge>
+                                  Technology
+                                </GreenBadge>
+                              </div>
+                              <div className="col-span-1">
+                                <p>Cash Flow</p>
+                                <RedBadge>
+                                  85%
+                                </RedBadge>
+                              </div>
+                              <div className="col-span-1">
+                                <p>Working Capital</p>
+                                <YellowBadge>
+                                  34%
+                                </YellowBadge>
+                              </div>
+                            </div>
+                          </div>
                         </div>
-                        <div>
-                          <dt className="text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0">Birthday</dt>
-                          <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">
-                            <time dateTime="1988-06-23">June 23, 1988</time>
-                          </dd>
-                        </div>
-                      </dl>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </Transition.Child>
-          </div>
-        </div>
-      </Dialog>
-    </Transition>
+            </Transition.Child >
+          </div >
+        </div >
+      </Dialog >
+    </Transition >
   )
 }
 
